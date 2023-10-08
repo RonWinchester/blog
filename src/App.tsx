7ba26ebfc2830a4dcab623fc785aaabc8feb5a08
Ron Winchester <rom0203@gmail.com>
2023-10-08 12:20:20 +0300

    feat: функция добавления классов

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,12 @@ import ErrorPage from "./pages/ErrorPage/ErrorPage";
 import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
 import { MainPageAsync } from "./pages/MainPage/MainPage.async";
 import useTheme from "./theme/useTheme";
+import { classNames } from "./helpers/classNames/classNames";
 
 export const App = () => {
     const {theme, toggleTheme } = useTheme()
 	return (
-		<div className={`app ${theme}`}>
+		<div className={classNames('app', {}, [theme])}>
 			<Link to={"/"}>Главная</Link>
 			<Link to={"/about"}>О сайте</Link>
 			<button onClick={toggleTheme}>Theme</button>
