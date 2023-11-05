76cb90d4d7f725f8dba80cc1777211bf39dbc3ab
Ron Winchester <rom0203@gmail.com>
2023-11-05 19:07:44 +0300

    feat: fix test sidebar

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -1,20 +1,11 @@
-import { fireEvent, screen } from "@testing-library/react";
-import { renderWithTranslation } from "shared/lib/tests/renderWithTranslation/renderWithTranslation";
+import { screen } from "@testing-library/react";
+import { ComponentRender } from "shared/lib/tests/componentRender/componentRender";
 import { Sidebar } from "./Sidebar";
 
 describe("Sidebar", () => {
 	test("have text", () => {
-		renderWithTranslation(<Sidebar />);
+		ComponentRender(<Sidebar />);
 		// eslint-disable-next-line i18next/no-literal-string
 		expect(screen.getByTestId("sidebar")).toBeInTheDocument();
-	});
-
-	test("toggle", () => {
-		renderWithTranslation(<Sidebar />);
-		const toggleButton = screen.getByTestId("sidebar-toggle");
-		// eslint-disable-next-line i18next/no-literal-string
-		expect(screen.getByTestId("sidebar")).toBeInTheDocument();
-		fireEvent.click(toggleButton);
-		expect(screen.getByTestId("sidebar")).toHaveClass("collapsed");
 	});
 });
