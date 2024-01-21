58af446a95a7cca7e023855e1d3e83ef887fc286
Ron Winchester <rom0203@gmail.com>
2024-01-21 22:23:39 +0300

    feat: асинхронная подгрузка модалки регистрации

--- src/app/providers/StoreProvider/config/store.ts
+++ src/app/providers/StoreProvider/config/store.ts
@@ -1,17 +1,26 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, ReducersMapObject } from "@reduxjs/toolkit";
 import { counterReducer } from "entities/Counter";
 import { userReducer } from "entities/User";
-import { loginReducer } from "features/authByUsername/model/slice/loginSlice";
+import { createReducerManager } from "./reducerManager";
 import { StateSchema } from "./StateSchema";
 
 export function createReduxStore(initialState?: StateSchema) {
-	return configureStore<StateSchema>({
-		reducer: {
-			counter: counterReducer,
-			user: userReducer,
-			loginForm: loginReducer,
-		},
+	const rootReducer: ReducersMapObject<StateSchema> = {
+		counter: counterReducer,
+		user: userReducer,
+	};
+
+	const reducerManager = createReducerManager(rootReducer);
+
+	const store = configureStore<StateSchema>({
+		reducer: reducerManager.reduce,
 		devTools: __IS__DEV__,
 		preloadedState: initialState,
 	});
+
+	// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+	// @ts-ignore
+	store.reducerManager = reducerManager;
+
+	return store;
 }
--- src/features/authByUsername/ui/LoginForm/LoginForm.stories.tsx
+++ src/features/authByUsername/ui/LoginForm/LoginForm.stories.tsx
@@ -1,7 +1,7 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { Theme } from "shared/config/theme/ThemeContext";
 import { ThemeDecorator } from "shared/config/storybook/ThemeDecorator/ThemeDecorator";
-import { LoginForm } from "./LoginForm";
+import LoginForm from "./LoginForm";
 import { StoreDecorator } from "shared/config/storybook/StoreDecorator/StoreDecorator";
 
 const meta: Meta<typeof LoginForm> = {
