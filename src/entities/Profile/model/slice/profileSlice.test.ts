dd5a248e6c0c1f143dc499d897fe5e9cae43fa6d
Ron Winchester <rom0203@gmail.com>
2024-04-01 21:45:22 +0300

    feat: тесты

---
+++ src/entities/Profile/model/slice/profileSlice.test.ts
@@ -0,0 +1,89 @@
+import { DeepPartial } from "@reduxjs/toolkit";
+import { profileReducer, profileActions } from "./profileSlice";
+import {
+	Profile,
+	ProfileSchema,
+	ValidetaProfileErrorCode,
+} from "../types/ProfileSchema";
+import { Country } from "entities/Country";
+import { Currency } from "entities/Currency";
+import { saveProfileData } from "../services/saveProfileData/saveProfileData";
+
+describe("profileSlice.test", () => {
+	const data = {
+		firstname: "Roman12333",
+		lastname: "lastname",
+		age: 2,
+		currency: Currency.USD,
+		country: Country.BY,
+		city: "spb",
+		username: "admin",
+		avatar: "https://mui.com/static/images/avatar/1.jpg",
+	};
+	test("return set readonly", () => {
+		const state: DeepPartial<ProfileSchema> = { readonly: true };
+		expect(
+			profileReducer(state as ProfileSchema, profileActions.setReadonly(true))
+		).toEqual({ readonly: true });
+	});
+	test("cancel Update", () => {
+		const state: DeepPartial<ProfileSchema> = {
+			data,
+			formData: { username: "" },
+		};
+		expect(
+			profileReducer(state as ProfileSchema, profileActions.cancelUpdate())
+		).toEqual({
+			readonly: true,
+			formData: data,
+			data,
+			validateErrors: undefined,
+		});
+	});
+	test("update Profile", () => {
+		const state: DeepPartial<ProfileSchema> = {
+			formData: {
+				firstname: "Roman12333",
+			},
+		};
+		expect(
+			profileReducer(
+				state as ProfileSchema,
+				profileActions.updateProfile({ firstname: "123" })
+			)
+		).toEqual({
+			formData: {
+				firstname: "123",
+			},
+		});
+	});
+	test("test update Profile server pending", () => {
+		const state: DeepPartial<ProfileSchema> = {
+			isLoading: false,
+			validateErrors: [ValidetaProfileErrorCode.INCOORECT_SERVER_ERROR],
+		};
+		expect(
+			profileReducer(state as ProfileSchema, saveProfileData.pending)
+		).toEqual({
+			isLoading: true,
+			validateErrors: undefined,
+		});
+	});
+	test("test update Profile server fulfilled", () => {
+		const state: DeepPartial<ProfileSchema> = {
+			isLoading: true,
+		};
+		expect(
+			profileReducer(
+				state as ProfileSchema,
+				saveProfileData.fulfilled(data as Profile, "")
+			)
+		).toEqual({
+			readonly: true,
+			isLoading: false,
+			data,
+			formData: data,
+			validateErrors: undefined,
+		});
+	});
+});
--- src/widgets/Navbar/ui/Navbar/Navbar.stories.tsx
+++ src/widgets/Navbar/ui/Navbar/Navbar.stories.tsx
@@ -1,5 +1,6 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { ThemeDecorator } from "shared/config/storybook/ThemeDecorator/ThemeDecorator";
+import { StoreDecorator } from "shared/config/storybook/StoreDecorator/StoreDecorator";
 import { Theme } from "shared/config/theme/ThemeContext";
 import { Navbar } from "./Navbar";
 
@@ -12,8 +13,34 @@ const meta: Meta<typeof Navbar> = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Light: Story = {};
+export const Light: Story = {
+	decorators: [
+		ThemeDecorator(Theme.LIGHT),
+		StoreDecorator({
+			user: {
+				authData: {
+					username: "123",
+					id: "123",
+				},
+			},
+		}),
+	],
+};
 
 export const Dark: Story = {
-	decorators: [ThemeDecorator(Theme.DARK)],
+	decorators: [
+		ThemeDecorator(Theme.DARK),
+		StoreDecorator({
+			user: {
+				authData: {
+					username: "123",
+					id: "123",
+				},
+			},
+		}),
+	],
+};
+
+export const NoAuth: Story = {
+	decorators: [ThemeDecorator(Theme.DARK), StoreDecorator({})],
 };
