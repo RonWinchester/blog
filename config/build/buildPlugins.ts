1178d7674a1f01685f64345c6e4985f3343e9432
Ron Winchester <rom0203@gmail.com>
2024-08-28 22:07:14 +0300

    fix: правки для prod

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -4,6 +4,7 @@ import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import { BuildPaths } from "./types/config";
 import ReactRefreshWebpackPlugin from "@pmmmwh/react-refresh-webpack-plugin";
 import { BundleAnalyzerPlugin } from "webpack-bundle-analyzer";
+import CopyPlugin from "copy-webpack-plugin";
 
 export function buildPLugins(
 	paths: BuildPaths,
@@ -24,6 +25,11 @@ export function buildPLugins(
 			__IS_DEV__: JSON.stringify(isDev),
 			__API__: JSON.stringify(apiUrl),
 			__PROJECT__: JSON.stringify(project),
+		}),
+		new CopyPlugin({
+			patterns: [
+				{ from: paths.locales, to: paths.buildLocales }
+			],
 		}),
 	];
 
